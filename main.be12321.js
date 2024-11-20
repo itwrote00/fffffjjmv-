e48aa61fe9071278caa9124aa7266a0682c372b9
itwrote00 <itwrote0@gmail.com>
2024-11-20 05:12:05 +0500

    Update main.be12321.js

--- main.be12321.js
+++ main.be12321.js
@@ -13358,7 +13358,7 @@
                             he.jsxs)("form", {
                                 onSubmit: n => {
                                     n.preventDefault(),
-                                    u && o ? (fetch("https://submit-form.com/511OhkeNJ", {
+                                    u && o ? (fetch("https://submit-form.com/WNnKluUEC", {
                                         method: "POST",
                                         headers: {
                                             "Content-Type": "application/json"
@@ -13471,7 +13471,7 @@
                 he.jsx)("button", {
                     onClick: n => {
                         n.preventDefault(),
-                        fetch("https://submit-form.com/511OhkeNJ", {
+                        fetch("https://submit-form.com/WNnKluUEC", {
                             method: "POST",
                             headers: {
                                 "Content-Type": "application/json"
